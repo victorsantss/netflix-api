a5df156978adb520a8a215a42f4568ad9d3db16c
Victor dos Santos <victo@Note-Victor.localdomain>
2022-06-17 12:12:30 -0300

    new custom and middleware for validation

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,14 +1,18 @@
-// import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request } from "express"
+import HTTP_STATUS from "../enums/http-status.enum"
+import HttpException from "../exceptions/http.exception"
+import { CustomResponse } from "../interfaces/custom-response.interface"
 
-import { Response } from "express"
+export const errorHandlerMiddleware = (request: Request, response: CustomResponse, next: NextFunction) => {
+  response.errorHandler = (e: any) => {
+    if (e instanceof HttpException) {
+      response
+        .status(e.status)
+        .send({ error: true, message: e.message, details: e })
+    } else {
+      response.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ error: true })
+    }
+  }
 
-const errorHandler = (e: any, response: Response) => {
-  response
-      .status(e.status)
-      .json({
-        error: true,
-        message: e.message
-      })
+  next()
 }
-
-export default errorHandler
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,15 +1,20 @@
+
 import express, { Application } from "express"
 import morgan from "morgan"
+import { errorHandlerMiddleware } from "../middlewares/error-handler.middleware"
 import ShowsRouter from "./shows.router"
 
 const routes = [
   ShowsRouter
 ]
+
 const jsonParserMiddleware = express.json()
 
 function startRoutes(app: Application) {
   app.use(jsonParserMiddleware)
   app.use(morgan('tiny'))
+
+  app.use(errorHandlerMiddleware)
 
   app.use(routes)
 }
